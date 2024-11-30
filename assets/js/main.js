aaa1f28b5722e727a832025ba2de1d71bbb920df
Fritz Ng <15712933+FritzNgKaaSheng@users.noreply.github.com>
2024-11-30 22:49:31 +0800

    [Update] PDF scaling

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,25 +5,42 @@ const invContainer = document.getElementById("inv-container");
 const jsPDFBtn = document.getElementById("jspdf-btn");
 const htmlDocxJsBtn = document.getElementById("html-docx-js-btn");
 
+const pdfOrientation = "portrait";
+const pdfSizeUnit = "px";
+// const a4SizeInPx = [width, height];
+const a4SizeInPx = [595, 842];
+const windowInnerWidth = window.innerWidth;
+
+const pdfExt = ".pdf";
+const docxExt = ".docx";
+const doctypeDeclaration = "<!DOCTYPE html>";
+
 function downloadPDF() {	
+	let docName = "a4";
+
 	var doc = new jsPDF({
-		orientation: "portrait",
-		unit: "mm",
-		format: [297, 210]
+		orientation: pdfOrientation,
+		unit: pdfSizeUnit,
+		format: a4SizeInPx
 	});   
 
 	doc.html(document.body, {
-	   callback: function (doc) {
-		 doc.save("a4.pdf");
-	   }
+		html2canvas: {
+			scale: (a4SizeInPx[0] / windowInnerWidth)
+		},
+		callback: function (doc) {
+			doc.save(docName + pdfExt);
+	  }
 	});
 };
 
 jsPDFBtn.addEventListener("click", downloadPDF);
 
 function downloadDOCX() {
-	var converted = htmlDocx.asBlob(document.querySelector("html").outerHTML);
-	saveAs(converted, 'a4.docx');
+	let docName = "a4";
+
+	var converted = htmlDocx.asBlob(doctypeDeclaration + document.querySelector("html").outerHTML);
+	saveAs(converted, docName + docxExt);
 }
 
 htmlDocxJsBtn.addEventListener("click", downloadDOCX);
