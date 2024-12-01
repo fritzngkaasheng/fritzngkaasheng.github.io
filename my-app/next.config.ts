41efe5d5ca6996734ad0970885cdc95a7fcff09b
Fritz Ng <15712933+FritzNgKaaSheng@users.noreply.github.com>
2024-12-01 16:05:50 +0800

    [Update] files to v0.0.2

--- my-app/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  basePath: "/my-app",
-  output: "export",
-};
-
-export default nextConfig;
