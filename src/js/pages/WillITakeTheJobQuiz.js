e7a1a4869a2b16aa030a447c287f4307bfbe9a44
Fritz Ng <15712933+FritzNgKaaSheng@users.noreply.github.com>
2025-01-20 00:34:06 +0800

    [Add] layout for Will I Take The Job quiz

--- src/js/pages/WillITakeTheJobQuiz.js
+++ src/js/pages/WillITakeTheJobQuiz.js
@@ -2,6 +2,40 @@
 
 import ComingSoonSection from "/src/js/components/ComingSoonSection.js";
 const WillITakeTheJobQuiz = () => {
-  return /*#__PURE__*/React.createElement(ComingSoonSection, null);
+  return /*#__PURE__*/React.createElement("div", {
+    className: "container"
+  }, /*#__PURE__*/React.createElement("form", null, /*#__PURE__*/React.createElement("div", {
+    className: "mb-3"
+  }, /*#__PURE__*/React.createElement("label", {
+    for: "quizOrigin",
+    className: "form-label"
+  }, "Which country is this job from?"), /*#__PURE__*/React.createElement("select", {
+    className: "form-select",
+    name: "origin",
+    id: "quizOrigin",
+    "aria-label": "Which country is this job from?"
+  }, /*#__PURE__*/React.createElement("option", {
+    selected: true
+  }, "Choose..."), /*#__PURE__*/React.createElement("option", {
+    value: "sg"
+  }, "Singapore"), /*#__PURE__*/React.createElement("option", {
+    value: "au"
+  }, "Australia"), /*#__PURE__*/React.createElement("option", {
+    value: "nz"
+  }, "New Zealand"), /*#__PURE__*/React.createElement("option", {
+    value: "cn"
+  }, "China"), /*#__PURE__*/React.createElement("option", {
+    value: "de"
+  }, "Germany"), /*#__PURE__*/React.createElement("option", {
+    value: "my"
+  }, "Malaysia"), /*#__PURE__*/React.createElement("option", {
+    value: "tw"
+  }, "Taiwan"), /*#__PURE__*/React.createElement("option", {
+    value: "gb"
+  }, "United Kingdom of Great Britain and Northern Ireland"), /*#__PURE__*/React.createElement("option", {
+    value: "th"
+  }, "Thailand"), /*#__PURE__*/React.createElement("option", {
+    value: "mm"
+  }, "Myanmar"))), /*#__PURE__*/React.createElement("h2", null, "The probability that I will choose this job is NaN%")));
 };
 export default WillITakeTheJobQuiz; 
--- src/jsx/pages/WillITakeTheJobQuiz.jsx
+++ src/jsx/pages/WillITakeTheJobQuiz.jsx
@@ -3,7 +3,29 @@
 import ComingSoonSection from "/src/js/components/ComingSoonSection.js";
 
 const WillITakeTheJobQuiz = () => {
-  return <ComingSoonSection />;
+  return (
+    <div className="container">
+      <form>
+        <div className="mb-3">
+          <label for="quizOrigin" className="form-label">Which country is this job from?</label>
+          <select className="form-select" name="origin" id="quizOrigin" aria-label="Which country is this job from?">
+            <option selected>Choose...</option>
+            <option value="sg">Singapore</option>
+            <option value="au">Australia</option>
+            <option value="nz">New Zealand</option>
+            <option value="cn">China</option>
+            <option value="de">Germany</option>
+            <option value="my">Malaysia</option>
+            <option value="tw">Taiwan</option>
+            <option value="gb">United Kingdom of Great Britain and Northern Ireland</option>
+            <option value="th">Thailand</option>
+            <option value="mm">Myanmar</option>
+          </select>
+        </div>
+        <h2>The probability that I will choose this job is NaN%</h2>
+      </form>
+    </div>
+  );
 };
 
 export default WillITakeTheJobQuiz;
