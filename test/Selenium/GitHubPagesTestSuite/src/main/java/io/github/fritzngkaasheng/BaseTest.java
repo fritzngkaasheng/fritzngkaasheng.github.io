1a80fe16e89c99c66d094027f4ed1fbb35588c85
Fritz Ng <15712933+FritzNgKaaSheng@users.noreply.github.com>
2026-04-21 22:47:27 +0800

    [Update] Bump app version and update build/tests
    Increment appVersion to 1.1.9 in config.js and include the regenerated production bundle (dist/js/main.bundle.min.js). Also updated test artifacts for the Selenium GitHub Pages suite (pom.xml and BaseTest.java) and refreshed package lock as part of the rebuild.

--- test/Selenium/GitHubPagesTestSuite/src/main/java/io/github/fritzngkaasheng/BaseTest.java
+++ test/Selenium/GitHubPagesTestSuite/src/main/java/io/github/fritzngkaasheng/BaseTest.java
@@ -1,17 +1,12 @@
 package io.github.fritzngkaasheng;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.fritzngkaasheng.tests.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.safari.SafariOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -25,7 +20,7 @@ public class BaseTest {
 
     private String browserName = "";
 
-    private String url = "https://localhost";
+    private String url = "https://myapp";
 
     public static boolean checkAllPagesTranslation = false;
 
